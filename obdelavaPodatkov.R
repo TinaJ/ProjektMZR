1a180bba6399ab2bfb3a093ad9955c1e9b47eaab
TinaJ <jansa.tina@gmail.com>
2013-11-19 18:53:54 +0100

    spremembe

--- obdelavaPodatkov.R
+++ obdelavaPodatkov.R
@@ -15,7 +15,6 @@ close(con)
 ## naložim podatke iz SP500.rda, če jih že imam shranjene - spremenjlivka SP500
 load("./SP500.rda")
 
-<<<<<<< HEAD
 ## potrebujem samo close vrednosti
 imena.Close = sapply(papirji, paste0, ".Close")
 podatki = SP500[, imena.Close]
@@ -70,65 +69,3 @@ data = podatki2[apply(podatki2, 1, function(x) sum(is.na(x)) == 0), ]
 ## shranim obdelane podatke v data.rda
 save(data,file="./data.rda")
 
-=======
-datumi = rownames(SP500)
-stolpci = colnames(SP500)
-
-## grem čez papirje in izločim tiste, ki ne obstajajo dost dolg, vsaj 10 let
-## če je 31.10.2003 NA
-meja = datumi == "2003-10-31" # vektor TRUE in FALSE, ali je datum pravi
-indeks = which(meja) # na katerem mestu je datum pravi
-
-niPodatka = is.na(SP500[meja,]) # vektor TRUE in FALSE, ali je v vrstici z danim datumom NA ali ne
-stolpciNA = stolpci[niPodatka] # imena stolpcev, kjer je NA pri danem datumu
-
-firstElement <- function(x){x[1]} # prvi element vsakega elementa v seznamu
-imena = sapply(strsplit(stolpciNA,"\\."),firstElement) # ločim imena stolpcev glede na piko in določim prvi element vsakega imena
-papirjiNA = unique(imena) # kateri papirji za 31.10.2003 nimajo podatka
-
-sum(!(is.na(SP500[1:indeks, niPodatka]))) # koliko podatkov ni NA pred danim datumom, za tiste stolpce, ki so na dani datum NA
-# če bi bila številka velika, bi bilo treba še kaj gledat, ker pa ni, ni treba
-
-# izbrišem stolpce, ki nimajo podatkov dovolj dolgo:
-tabela = SP500[, !niPodatka]
-rownames(tabela) = datumi
-
-## podatki za 5 let in 10 let:
-"2008-11-01" %in% datumi
-"2008-10-31" %in% datumi
-let5 = which(datumi == "2008-10-31")
-podatki5let = tabela[let5: nrow(tabela), ]
-
-"2003-11-01" %in% datumi
-"2003-10-31" %in% datumi
-let10 = which(datumi == "2003-10-31")
-podatki10let = tabela[let10:nrow(tabela),]
-
-## shranim tabelo, ter podatke za 5 in 10 let v datoteko:
-save(tabela, podatki5let, podatki10let,file="./podatki.rda")
-
-## naložim podatke
-load("./podatki.rda")
-
-
-############################################################
-load("./podatki.rda")
-## delam na podatkih v spremenljivki tabela
-## za vsak papir potrebujem samo close ceno
-## naredim seznam imen papirjev, ki so v tabela:
-# prvi element vsakega elementa v seznamu:
-firstElement <- function(x){x[1]} ,
-# ločim imena stolpcev glede na piko in določim prvi element vsakega imena, vzamem samo eno ponovitev vsakega imena:
-imena = unique(sapply(strsplit(colnames(tabela),"\\."),firstElement))
-# imenom dodam ".Close"
-imena.Close = sapply(imena, paste0, ".Close")
-
-imena.Close %in% colnames(tabela)
-imena.Close[!(imena.Close %in% colnames(tabela))] # katera so v imena.Close in niso v tabeli
-# --> BRK.Close in BF.Close ## zakaj???
-imena.CloseOK = imena.Close[imena.Close %in% colnames(tabela)] # tisti papirji, ki imajo close vrednost v tabeli
-data = tabela[, imena.CloseOK] # za vsak papir, ki pride v poštev samo close vrednost
-save(data, file = "./CloseVrednosti2000.rda")
-
-##############################################################3
->>>>>>> parent of 5bfcc15... popravki
