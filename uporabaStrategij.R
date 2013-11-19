e6b18a759e5630e873be2a350d4f64758d4d5d90
TinaJ <jansa.tina@gmail.com>
2013-11-19 19:22:01 +0100

    spremenjena uporaba strategij, a še ne uspešno

--- uporabaStrategij.R
+++ uporabaStrategij.R
@@ -1,13 +1,29 @@
 setwd("C:/Users/Tina/Documents/faks/2. letnik magisterija/matematika z računalnikom/ProjektMZR")
 
 # podatki za close vrednost za vse papirje, ki so dovolj dolgo v S&P, od 3.1.2000 do 1.11.2013
-load("./CloseVrednosti2000.rda")
-load("./CloseVrednosti2003.rda")
-load("./CloseVrednosti2008.rda")
+load("./data.rda")
+
+# za podatke izračunam indekse: SMA5, 25, 50, 150, RSI 2, 14, bollinger za n = 20 in faktor = 2
+SMA5 = apply(data, 2, SMA, n = 5)
+SMA25 = apply(data, 2, SMA, n = 25)
+SMA50 = apply(data, 2, SMA, n = 50)
+SMA150 = apply(data, 2, SMA, n = 150)
+
+RSI2 = apply(data, 2, RSI, n = 2)
+RSI14= apply(data, 2, RSI, n = 14)
+
+bollinger = apply(data, 2, BBands, n = 20, sd = 2) 
+# => v isti stolpec zloži dn, mavg, up, pctB enega za drugim, ločit jih moraš
 
 # najprej moraš pognati kodo v strategije.R
 
-# donosi za vsak papir v data5, za dane strategije
+# matrika izgub in dobička za vsak papir v data, za dane strategije
+# vsaka strategija ima svojo matriko, vsak stolpec v eni matriki je en papir
+
+## rabm funkcijo, ki iz dveh matrik vzame istoležne stolpce in na njih uporabi strategijo?!
+
+
+
 bollinger = apply(data5, 2, BollingerStrategy, n = 20, faktor = 2, budget = 1000)
 buyHold = apply(data5, 2, BuyHoldStrategy, n = 1, budget = 1000)
 RSI2 = apply(data5, 2, RSIstrategy, n = 2, budget = 1000)
